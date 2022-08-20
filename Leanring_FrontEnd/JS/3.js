523894ada7dd9593d5eb17135eaf5e85807d0eef
UncleTaoPaiPai <yongzhi.zhou@protonmail.com>
2022-08-21 09:53:26 +1000

    frond end day06

--- Leanring_FrontEnd/JS/3.js
+++ Leanring_FrontEnd/JS/3.js
@@ -4,6 +4,17 @@ function Factory(color,height,suv) {
     this.suv=suv;
 }
 
-var car1= new Factory("RED",1,true);
-console.log(car1.color)
-console.log(car1)
+// var car1= new Factory("RED",1,true);
+// console.log(car1.color)
+// console.log(car1)
+
+function Car(obj){
+    this.color=obj.color;
+    this.height=obj.height;
+    this.suv=obj.suv;
+}
+
+var audi=new Car({color: "small",height:"audi",suv:11});
+console.log(audi);
+
+
